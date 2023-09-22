88855a3dda996e40b60178076189ba6c0ed616bd
boagnnnn <diablomj@hotmail.fr>
2023-09-22 11:54:06 +0200

    up

--- js/javascript.js
+++ js/javascript.js
@@ -14,29 +14,67 @@
 //    }
 //}
 
+function plus_i(){
+    document.getElementById("menu").src="images/Ligne 6.png";
+}
+function moins_i(){
+    document.getElementById("menu").src="images/Composant 1 – 6.png";
+}
+function plus_1_i(){
+    document.getElementById("menu_1").src="images/Ligne 6.png";
+}
+function moins_1_i(){
+    document.getElementById("menu_1").src="images/Composant 1 – 6.png";
+}
+function plus_2_i(){
+    document.getElementById("menu_2").src="images/Ligne 6.png";
+}
+function moins_2_i(){
+    document.getElementById("menu_2").src="images/Composant 1 – 6.png";
+}
+function plus_3_i(){
+    document.getElementById("menu_3").src="images/Ligne 6.png";
+}
+function moins_3_i(){
+    document.getElementById("menu_3").src="images/Composant 1 – 6.png";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function plus(){
-    document.getElementById("menu").src="/images/Ligne 6.png";
+    document.getElementById("menu").src="../images/Ligne 6.png";
 }
 function moins(){
-    document.getElementById("menu").src="/images/Composant 1 – 6.png";
+    document.getElementById("menu").src="../images/Composant 1 – 6.png";
 }
 function plus_1(){
-    document.getElementById("menu_1").src="/images/Ligne 6.png";
+    document.getElementById("menu_1").src="../images/Ligne 6.png";
 }
 function moins_1(){
-    document.getElementById("menu_1").src="/images/Composant 1 – 6.png";
+    document.getElementById("menu_1").src="../images/Composant 1 – 6.png";
 }
 function plus_2(){
-    document.getElementById("menu_2").src="/images/Ligne 6.png";
+    document.getElementById("menu_2").src="../images/Ligne 6.png";
 }
 function moins_2(){
-    document.getElementById("menu_2").src="/images/Composant 1 – 6.png";
+    document.getElementById("menu_2").src="../images/Composant 1 – 6.png";
 }
 function plus_3(){
-    document.getElementById("menu_3").src="/images/Ligne 6.png";
+    document.getElementById("menu_3").src="../images/Ligne 6.png";
 }
 function moins_3(){
-    document.getElementById("menu_3").src="/images/Composant 1 – 6.png";
+    document.getElementById("menu_3").src="../images/Composant 1 – 6.png";
 }
 
 function sh(){
